5d1d7af72d13b52a2de2a4c794bf5b3d4eea9b4f
zongheng23333 <1954640276@qq.com>
2024-07-09 19:36:21 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,6 +4,10 @@
 rm -rf feeds/luci/applications/luci-app-openclash
 cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
 
+rm -rf feeds/luci/applications/luci-app-frpc
+git clone --depth=1 --single-branch --branch   master      "https://github.com/kuoruan/luci-app-frpc"
+cp -rf    luci-app-frpc  feeds/luci/applications/luci-app-frpc
+
 
 rm -rf  feeds/packages/net/frp
 git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp" 
