dce3a744eaf1621e89ad0ae68b71985201734a7b
zongheng23333 <1954640276@qq.com>
2024-07-10 08:59:10 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -8,8 +8,8 @@ cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-ope
 
 
 rm -rf  feeds/packages/net/frp
-git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp" 
-cp -rf  openwrt-frp  feeds/packages/net/frp
+git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp" feeds/packages/net/frp
+
 rm -rf feeds/luci/applications/luci-app-frpc
 git clone https://github.com/kuoruan/luci-app-frpc feeds/luci/applications/luci-app-frpc
 sed -i -e 's/\tlocal frp_version=.*/\tlocal frp_version='''$FRP_VERSION'''/' feeds/luci/applications/luci-app-frpc/root/etc/init.d/frp
