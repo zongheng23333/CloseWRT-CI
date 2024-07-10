23bfbdf443e1754d84eede4f3b1c30480f378ec2
zongheng23333 <1954640276@qq.com>
2024-07-10 12:56:25 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-
-
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ 
 ./scripts/feeds update -a 
 rm -rf feeds/luci/applications/luci-app-openclash
 cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
@@ -15,11 +13,7 @@ git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/ku
 git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp" feeds/smpackage/frp
 
 rm -rf feeds/luci/applications/luci-app-frpc
-
 git clone https://github.com/kuoruan/luci-app-frpc       feeds/luci/applications/luci-app-frpc
-sed -i -e 's/\tlocal frp_version=.*/\tlocal frp_version='''$FRP_VERSION'''/' feeds/luci/applications/luci-app-frpc/root/etc/init.d/frp
 
 
 ./scripts/feeds install -a  
-
-make clean 
