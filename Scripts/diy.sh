7d9040f03074f989497d3b11b4d95ada4a4b0845
zongheng23333 <1954640276@qq.com>
2024-07-11 14:45:59 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
- echo >> feeds.conf.default
+sed -i '1i src-git mult https://github.com/justice2001/luci-app-multi-frpc' feeds.conf.default
+sed -i '2i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo >> feeds.conf.default
 
 
 ./scripts/feeds update -a 
@@ -10,10 +11,12 @@ cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-ope
 
 rm -rf  feeds/packages/net/frp
 git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp" feeds/packages/net/frp
-git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp" feeds/smpackage/frp
 
 
 
+rm -rf feeds/luci/applications/luci-app-frpc
+git clone https://github.com/kuoruan/luci-app-frpc       feeds/luci/applications/luci-app-frpc
+git clone https://github.com/justice2001/luci-app-multi-frpc   feeds/luci/applications/luci-app-multi-frpc
 
 
 
