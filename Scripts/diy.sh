aadcc495045bed2fa47a26c3678a7d6c40eecf77
zongheng23333 <1954640276@qq.com>
2024-07-12 14:53:33 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-sed -i '2i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-echo >> feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
+
+rm -rf  feeds/packages/net/frp
+
+rm -rf feeds/luci/applications/luci-app-frpc
+
+git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp"      package/zongheng/frp
+
+git clone --depth=1 --single-branch --branch   v1.2.1-1  "https://github.com/kuoruan/luci-app-frpc"       package/zongheng/luci-app-frpc
+#git clone --depth=1 --single-branch --branch   main     "https://github.com/simplebean/luci-app-frpc"    package/zongheng/luci-app-frpc
+#git clone --depth=1 --single-branch --branch   main     "https://github.com/justice2001/luci-app-multi-frpc"    package/zongheng/luci-app-multi-frpc
 
 ./scripts/feeds update -a 
 rm -rf feeds/luci/applications/luci-app-openclash
@@ -11,16 +20,7 @@ cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-ope
 ./scripts/feeds install -a  
 
 
-rm -rf  feeds/packages/net/frp
 
-rm -rf feeds/luci/applications/luci-app-frpc
-
-git clone --depth=1 --single-branch --branch   v0.42.0-1  "https://github.com/kuoruan/openwrt-frp"      package/zongheng/frp
-
-
-#git clone --depth=1 --single-branch --branch   main     "https://github.com/simplebean/luci-app-frpc"    package/zongheng/luci-app-frpc
-#git clone --depth=1 --single-branch --branch   main     "https://github.com/justice2001/luci-app-multi-frpc"    package/zongheng/luci-app-multi-frpc
-git clone --depth=1 --single-branch --branch   v1.2.1-1  "https://github.com/kuoruan/luci-app-frpc"       package/zongheng/luci-app-frpc
 
  
 
