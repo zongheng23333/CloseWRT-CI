bb6bdeb7c2e667021d94be6af194668ac3b74fed
zongheng23333 <1954640276@qq.com>
2024-09-05 21:16:11 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
  echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
+ 
  git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+ git clone https://github.com/lucikap/luci-app-ua2f package/luci-app-ua2f
+ git clone https://github.com/Zxilly/UA2F package/UA2F 
+
+ 
 ./scripts/feeds update -a 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
